cd32e9632dcffb82bd0735107d3ec7e5dcb486fe
Sebastian Gfeller <sebastian.gfeller@gmail.com>
2023-06-23 15:06:32 +0000

    Trying again, but this time with sudo tee

--- ha_post_install.sh
+++ ha_post_install.sh
@@ -1,9 +1,8 @@
 sudo dnf -y install haproxy
 sudo systemctl start haproxy
 sudo systemctl enable haproxy
-curl -O https://raw.githubusercontent.com/sebug/ha-phang/main/haproxy.cfg
+curl https://raw.githubusercontent.com/sebug/ha-phang/main/haproxy.cfg | sudo tee /etc/haproxy/haproxy.cfg
 sudo ls -l /etc/haproxy/haproxy.cfg
-sudo mv haproxy.cfg /etc/haproxy/haproxy.cfg
 sudo sed -i '/"imudp/s/^#//g' /etc/rsyslog.conf
 echo -e "local2.=info\t/var/log/haproxy-access.log" | sudo tee  /etc/rsyslog.d/haproxy.conf
 echo -e "local2.notice\t/var/log/haproxy-info.log" | sudo tee -a /etc/rsyslog.d/haproxy.conf
